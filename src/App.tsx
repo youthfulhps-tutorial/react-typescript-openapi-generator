358ceeae20fffbcf7648c3a7d7c04908ba9d4f4b
BH94 <ybh942002@gmail.com>
2021-04-04 21:23:39 +0900

    created pet api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { AxiosResponse } from "axios";
+import { PetApi, Pet } from "./lib/apis/.";
 
 function App() {
+  const petApi = new PetApi();
+
+  const handleButtonClick = async () => {
+    const response: AxiosResponse<Pet> = await petApi.getPetById(1);
+    console.log(response.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleButtonClick}>Pet!</button>
     </div>
   );
 }
